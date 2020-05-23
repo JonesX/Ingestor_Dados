3a0b098a741c19211150fd2023737964a77adf4a
Jones Vital <jonesvital@gmail.com>
2020-05-23 02:06:36 -0300

    Inclusão dos parâmetros de inicialização

--- Ingestor_dados_FINAL.py
+++ Ingestor_dados_FINAL.py
@@ -15,62 +15,76 @@ from json_tricks import dump, dumps, load, loads, strip_comments
 from multiprocessing.dummy import Pool as ThreadPool
 
 def cut_send(name):
-        print("Analisando imagem "+name)
-        h, w, xOffSet, yOffSet = 120, 120, 60, 60
-        path_imagens = "/media/administrador/SuperLeggera:0/Imagens_Habilitadas/imagens/"
-        path_imagem = path_imagens+name
-        imgOri = cv2.imread(path_imagem)
-        ds = gdal.Open(path_imagem)
-        producer = KafkaProducer(bootstrap_servers=['192.168.25.102:9092'])
+    print("Analisando imagem "+name)
+    h, w, xOffSet, yOffSet = 120, 120, 60, 60
+    path_imagens = "/media/administrador/SuperLeggera:0/Imagens_Habilitadas/imagens/"
+    path_imagem = path_imagens+name
+    imgOri = cv2.imread(path_imagem)
+    ds = gdal.Open(path_imagem)
+    producer = KafkaProducer(bootstrap_servers=['192.168.25.102:9092'])
 
-        xOrigin, px_w, rot1, yOrigin, rot2, px_h = ds.GetGeoTransform()
+    xOrigin, px_w, rot1, yOrigin, rot2, px_h = ds.GetGeoTransform()
 
-        imagem = cv2.cvtColor(imgOri, cv2.COLOR_BGR2GRAY)
+    imagem = cv2.cvtColor(imgOri, cv2.COLOR_BGR2GRAY)
 
-        Y = imagem.shape[0] #Altura da imagem
-        X = imagem.shape[1] #Largura da imagem
+    Y = imagem.shape[0] #Altura da imagem
+    X = imagem.shape[1] #Largura da imagem
 
-        y0, y1 = 0, h
-        
-        while Y >= y1:
-            x0, x1 = 0, w
+    y0, y1 = 0, h
+    
+    while Y >= y1:
+        x0, x1 = 0, w
 
-            while X >= x1:
-                imageCropped = imagem[y0:y1, x0:x1]
-                
-                json_imagem = {
-                    "nome_arquivo":name,
-                    "projection_ref":ds.GetProjectionRef(),
-                    "x_origin":xOrigin,
-                    "y_origin":yOrigin,
-                    "px_w":px_w,
-                    "px_h":px_h,
-                    "X0":x0,
-                    "Y0":y0,
-                    "imagem":dumps(imageCropped)
-                }
-                
-                '''
-                json_imagem = {
-                    "nome_arquivo":name,
-                    "ds":ds.,
-                    "imagem":dumps(imageCropped)
-                }
-                '''
-                json_object = dumps(json_imagem)
+        while X >= x1:
+            imageCropped = imagem[y0:y1, x0:x1]
+            
+            json_imagem = {
+                "nome_arquivo":name,
+                "projection_ref":ds.GetProjectionRef(),
+                "x_origin":xOrigin,
+                "y_origin":yOrigin,
+                "px_w":px_w,
+                "px_h":px_h,
+                "X0":x0,
+                "Y0":y0,
+                "imagem":dumps(imageCropped)
+            }
+            
+            '''
+            json_imagem = {
+                "nome_arquivo":name,
+                "ds":ds.,
+                "imagem":dumps(imageCropped)
+            }
+            
 
-                producer.send('landingqueue',bytes(json_object,'utf-8'))
-                producer.flush()
+            json_imagem = {
+                "nome_arquivo":name,
+                "projection_ref":ds.GetProjectionRef(),
+                "x_origin":xOrigin,
+                "y_origin":yOrigin,
+                "px_w":px_w,
+                "px_h":px_h,
+                "X0":x0,
+                "Y0":y0
+            }
+            '''
+            #print(json_imagem)
 
-                x0, x1 = (x0+xOffSet), (x1+xOffSet)
+            json_object = dumps(json_imagem)
 
-            y0, y1 = (y0+yOffSet),(y1+yOffSet)
+            producer.send('landingqueue',bytes(json_object,'utf-8'))
+            producer.flush()
+
+            x0, x1 = (x0+xOffSet), (x1+xOffSet)
+
+        y0, y1 = (y0+yOffSet),(y1+yOffSet)
 
 def main():
     path_imagens = "/media/administrador/SuperLeggera:0/Imagens_Habilitadas/imagens/"
     imagens_pistas = listdir(path_imagens)
 
-    pool = ThreadPool(1)
+    pool = ThreadPool(2)
     results = pool.map(cut_send, imagens_pistas)
     pool.close()
     pool.join()
